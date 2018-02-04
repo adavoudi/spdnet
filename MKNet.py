026cde1b3ef84f9f3635c50b92d2efad6a41db4a
Alireza Davoudi <davoudialireza@gmail.com>
2018-02-04 23:55:51 +0330

    Added subtract_mean to kernels

--- MKNet.py
+++ MKNet.py
@@ -4,16 +4,23 @@ from torch.autograd import Variable, Function
 
 import numpy as np
 
+def is_nan_or_inf(A):
+    C1 = torch.nonzero(A == float('inf'))
+    C2 = torch.nonzero(A != A)
+    if len(C1.size()) > 0 or len(C2.size()) > 0:
+        return True
+    return False
 
 class PolynomialKernel(nn.Module):
 
-    def __init__(self, num_input_features, use_center=False, degree=1, added_value=0, center_init_scale=1):
+    def __init__(self, num_input_features, subtract_mean=True, use_center=False, degree=1, added_value=0, center_init_scale=1):
         super(PolynomialKernel, self).__init__()
         
         self.degree = degree
         self.added_value = added_value
         self.num_input_features = num_input_features
         self.use_center = use_center
+        self.subtract_mean = subtract_mean
 
         if use_center:
             self.center = nn.Parameter(torch.FloatTensor([num_input_features, 1]), requires_grad=True)
@@ -33,8 +40,14 @@ class PolynomialKernel(nn.Module):
             center = self.center.expand(-1, input.size(2))
 
         for k, x in enumerate(input):
+            if self.subtract_mean:
+                mean = torch.mean(x, 0, True)
+                mean = mean.expand(x.size(0), -1)
+                x = x - mean
+
             if self.use_center:
                 x = x - center
+                
             P = x.mm(x.t())
             P = P.add(self.added_value)
             P = P.pow(self.degree)
@@ -45,7 +58,7 @@ class PolynomialKernel(nn.Module):
 
 class GaussianKernel(nn.Module):
 
-    def __init__(self, num_input_features, use_center=False, kernel_width=None, laplacian_kernel=False, center_init_scale=1):
+    def __init__(self, num_input_features, subtract_mean=True, use_center=False, kernel_width=None, laplacian_kernel=False, center_init_scale=1):
         """
         Arguments:
             kernel_width {float} -- -0.5/sigma^2 if 
@@ -57,6 +70,7 @@ class GaussianKernel(nn.Module):
         super(GaussianKernel, self).__init__()
 
         self.num_input_features = num_input_features
+        self.subtract_mean = subtract_mean
         self.use_center = use_center
         self.kernel_width = kernel_width
         self.laplacian_kernel = laplacian_kernel
@@ -66,7 +80,7 @@ class GaussianKernel(nn.Module):
             nn.init.constant(self.kernel_width, 1)
         
         if use_center:
-            self.center = nn.Parameter(torch.FloatTensor([num_input_features, 1]), requires_grad=True)
+            self.center = nn.Parameter(torch.FloatTensor([num_input_features]), requires_grad=True)
             nn.init.uniform(self.center, a=-1*center_init_scale, b=1*center_init_scale)
 
     def forward(self, input):
@@ -80,11 +94,18 @@ class GaussianKernel(nn.Module):
 
         output = input.new(input.size(0), self.num_input_features, self.num_input_features)
         if self.use_center:
-            center = self.center.expand(-1, input.size(2))
+            center = self.center.unsqueeze(1)
+            center = center.expand(-1, input.size(2))
 
         for k, x in enumerate(input):
+            if self.subtract_mean:
+                mean = torch.mean(x, 0, True)
+                mean = mean.expand(x.size(0), -1)
+                x = x - mean
+
             if self.use_center:
                 x = x - center
+            
             P1 = x.mm(x.t())
             P2 = P1.diag(0)
             P2 = P2.unsqueeze(1)
@@ -93,12 +114,12 @@ class GaussianKernel(nn.Module):
             P2 = P2 - 2*P1
             if self.laplacian_kernel:
                 P2 = P2.sqrt()
-            output[k] = torch.exp(-self.kernel_width*P2)
+            output[k] = torch.exp(-1 * self.kernel_width*P2)
 
         return output
  
 
- class MixKernel(nn.Module):
+class MixKernel(nn.Module):
 
     def __init__(self, is_product=False, use_weight_for_a=False, use_weight_for_b=False):
 
