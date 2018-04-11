3e95d662754256d17912fbdd5b8b9326b7b52cbd
Alireza Davoudi <davoudialireza@gmail.com>
2018-04-11 17:32:36 +0430

    Added new VAE arch

--- VAE.py
+++ VAE.py
@@ -13,11 +13,32 @@ from dataset import *
 from SPDNet import *
 from MKNet import *
 
-transformed_dataset = AfewDataset('/home/alireza/projects/TF_SPDNet/data/AFEW', '/home/alireza/projects/SPDNet/data/afew/spddb_afew_train_spd400_int_histeq.mat', train=True)
+def determinant(A):
+    """
+        returns the determinant of spd matrix A
+    """
+    # try:
+    output = torch.potrf(A).diag().prod()
+    # except:
+        # print(is_pos_def(A.data))
+    return output
+
+def distance_kullback(A, B):
+    """Kullback leibler divergence between two covariance matrices A and B.
+    :param A: First covariance matrix
+    :param B: Second covariance matrix
+    :returns: Kullback leibler divergence between A and B
+    """
+    dim = A.size(0)
+    logdet = torch.log(determinant(A) / determinant(B))
+    kl = torch.mm(B.inverse(), A).trace() - dim + logdet
+    return 0.5 * kl
+
+transformed_dataset = AfewDataset('./data/AFEW', './data/AFEW/spddb_afew_train_spd400_int_histeq.mat', train=True)
 dataloader = DataLoader(transformed_dataset, batch_size=30,
                     shuffle=True, num_workers=4)
 
-transformed_dataset_val = AfewDataset('/home/alireza/projects/TF_SPDNet/data/AFEW', '/home/alireza/projects/SPDNet/data/afew/spddb_afew_train_spd400_int_histeq.mat', train=False)
+transformed_dataset_val = AfewDataset('./data/AFEW', './data/AFEW/spddb_afew_train_spd400_int_histeq.mat', train=False)
 dataloader_val = DataLoader(transformed_dataset_val, batch_size=30,
                     shuffle=False, num_workers=4)
 
@@ -29,95 +50,99 @@ class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
 
-        self.trans1 = SPDTransform(400, 50)
-        # self.trans2 = SPDTransform(200, 100)
-        # self.trans3 = SPDTransform(100, 20)
-        # self.rec1 = SPDRectified()
-        # self.rec2 = SPDRectified()
-        self.tangent = SPDTangentSpace(True) 
-        self.bn =  nn.BatchNorm1d(1275)
-        # self.bn0 =  nn.BatchNorm1d(1275)
-        self.fc21 = nn.Linear(1275, 40, bias=True)
-        self.fc22 = nn.Linear(1275, 40, bias=True)
-        self.fc3 = nn.Linear(40, 400)
-        self.fc4 = nn.Linear(400, 1275)
-        self.fc5 = nn.Linear(1275, 80200)
-        self.linear = nn.Linear(1275, 7, bias=True)
+        self.enc_trans1 = SPDTransform(400, 200)
+        self.enc_trans2 = SPDTransform(200, 100)
+        self.enc_trans3 = SPDTransform(100, 50)
+        self.enc_rec1 = SPDRectified()
+        self.enc_rec2 = SPDRectified()
+        self.enc_rec3 = SPDRectified()
+
+        self.dec_trans1 = SPDTransform(50, 100)
+        self.dec_trans2 = SPDTransform(100, 200)
+        self.dec_trans3 = SPDTransform(200, 400)
+        self.dec_rec1 = SPDRectified()
+        self.dec_rec2 = SPDRectified()
+        self.dec_rec3 = SPDRectified()
 
     def encode(self, x):
-        x = self.trans1(x)
-        # x = self.rec1(x)
-        # x = self.trans2(x)
-        # x = self.rec2(x)
-        # x = self.trans3(x)
-        x = self.tangent(x)
-        x = self.bn(x)
-        return self.fc21(x), self.fc22(x), self.linear(x)
+        x = self.enc_trans1(x)
+        x = self.enc_rec1(x)
+        x = self.enc_trans2(x)
+        x = self.enc_rec2(x)
+        x = self.enc_trans3(x)
+        x = self.enc_rec3(x)
+        return x
 
-    def reparametrize(self, mu, logvar):
-        std = logvar.mul(0.5).exp_()
-        eps = torch.FloatTensor(std.size()).normal_()
-        eps = Variable(eps)
-        return eps.mul(std).add_(mu)
+    def decode(self, x):
+        x = self.dec_trans1(x)
+        print('1:', is_pos_def(x[0].data))
+        x = self.dec_rec1(x)
+        x = self.dec_trans2(x)
+        print('2:', is_pos_def(x[0].data))
+        x = self.dec_rec2(x)
+        x = self.dec_trans3(x)
+        print('3:', is_pos_def(x[0].data))
+        x = self.dec_rec3(x)
 
-    def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        h4 = F.relu(self.fc4(h3))
-        return self.fc5(h4)
+        return x
 
     def forward(self, x):
-        mu, logvar, clsi = self.encode(x)
-        z = self.reparametrize(mu, logvar)
-        return self.decode(z), mu, logvar, clsi
+        enc = self.encode(x)
+        dec = self.decode(enc)
+        return dec, enc
 
 model = VAE()
-model.load_state_dict(torch.load('./vae.pth'))
+# model.load_state_dict(torch.load('./vae.pth'))
 
-criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters())
 optimizer = StiefelMetaOptimizer(optimizer)
 
-reconstruction_function = nn.MSELoss(size_average=False)
-tangent_space = SPDTangentSpace(True)
-criterion_cls = nn.CrossEntropyLoss()
+def loss_function(recon_x, orig_x, encoded_x):
 
-def loss_function(recon_x, x, mu, logvar, clsi, clsorg):
-    """
-    recon_x: generating images
-    x: origin images
-    mu: latent mean
-    logvar: latent log variance
-    """
-    BCE = reconstruction_function(recon_x, tangent_space(x))  # mse loss
-    # loss = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
-    KLD = torch.sum(KLD_element).mul_(-0.5)
-    loss_cls = criterion(clsi, clsorg)
-    # KL divergence
-    return BCE + KLD + loss_cls
+    identity = Variable(torch.eye(encoded_x.size(1),encoded_x.size(1)), requires_grad=False)
+
+    kl_loss_reconstruction = 0
+    for index in range(recon_x.size(0)):
+        A = orig_x[index]
+        B = recon_x[index]
+        # print('A:', is_pos_def(A.data))
+        # print('B:', is_pos_def(B.data))
+        kl_loss_reconstruction -= distance_kullback(A, B)
+
+    kl_loss_encoding = 0
+    for index in range(encoded_x.size(0)):
+        kl_loss_encoding -= distance_kullback(encoded_x[index], identity)
+    
+    loss = kl_loss_encoding + kl_loss_reconstruction
+    
+    return loss
 
 try:
     for epoch in range(num_epochs):
         model.train()
         train_loss = 0
         for batch_idx, data in enumerate(dataloader):
-            img = Variable(data['data'])
-            target = Variable(data['label']).squeeze()
+            inputs = Variable(data['data'], requires_grad=False)
+            # targets = Variable(data['label']).squeeze()
+            
+            dec, enc = model(inputs)
+            loss = loss_function(dec, inputs, enc)
+            
             optimizer.zero_grad()
-            recon_batch, mu, logvar, clsi = model(img)
-            loss = loss_function(recon_batch, img, mu, logvar, clsi, target)
             loss.backward()
             train_loss += loss.data[0]
             optimizer.step()
+
             if batch_idx % 10 == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch,
-                    batch_idx * len(img),
+                    batch_idx * len(inputs),
                     len(dataloader.dataset), 100. * batch_idx / len(dataloader),
-                    loss.data[0] / len(img)))
+                    loss.data[0] / len(inputs)))
 
         print('====> Epoch: {} Average loss: {:.4f}'.format(
             epoch, train_loss / len(dataloader.dataset)))
+
 except KeyboardInterrupt:
     pass
 
